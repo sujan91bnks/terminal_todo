00d1dbafa67a57ed2bb8f6e16021809debbc9eea
Sujan Thapa <10thapasujan91@gmail.com>
2026-04-26 12:07:56 -0400

    feat: add list and delete

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <sqlite3.h>
 #include <string.h>
 
+
 int callback(void *data, int argc, char **argv, char **azColName) {
-    for (int i = 0; i < argc; i++) {
-        printf(" %s %s %s", "[","]", argv[i] ? argv[i] : "NULL");
+        if (argv[0]) {
+        printf("[] %s", argv[0]);
     }
-    printf("\n");
     return 0;
 }
 
@@ -15,9 +15,7 @@ void parse_input(char input_text[100], sqlite3 *db) {
 
     if (strcmp(input_text, "ls\n") == 0) {
 
-        printf(" TO  DO  LIST\n");
-
-        char *get_task = "SELECT Task FROM todolist WHERE Status=1";
+        char *get_task = "SELECT  Task FROM todolist WHERE Status=1";
 
         int rc = sqlite3_exec(db, get_task, callback, 0, &errMsg);
 
@@ -43,6 +41,9 @@ void parse_input(char input_text[100], sqlite3 *db) {
             strcat(result, " ");
             token = strtok(NULL, " ");
         }
+        if (strlen(result) > 0) {
+            result[strlen(result) - 1] = '\0';
+        }
 
         char *add_command =
             "INSERT INTO todolist(Task, Topic, Status) VALUES(?, 'maintopic', 1)";
@@ -58,13 +59,50 @@ void parse_input(char input_text[100], sqlite3 *db) {
 
         sqlite3_finalize(stmt);
     }
+    else if (strncmp(input_text, "rm",2) == 0) {
+    sqlite3_stmt *stmt;
+
+    char result[100] = "";
+
+    char *token = strtok(input_text, " "); // skip "del"
+    token = strtok(NULL, " ");
+
+    while (token != NULL) {
+        strcat(result, token);
+        strcat(result, " ");
+        token = strtok(NULL, " ");
+    }
+    if (strlen(result) > 0 && result[strlen(result) - 1] == ' ') {
+    result[strlen(result) - 1] = '\0';
 }
+
+    char *delete_cmd = "DELETE FROM todolist WHERE Task LIKE ?";
+
+    sqlite3_prepare_v2(db, delete_cmd, -1, &stmt, NULL);
+
+    // add % for partial match
+    char pattern[120];
+    sprintf(pattern, "%%%s%%", result);
+
+    sqlite3_bind_text(stmt, 1, pattern, -1, SQLITE_TRANSIENT);
+
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        printf("Delete failed\n");
+    } else {
+        printf("Deleted matching tasks\n");
+    }
+
+    sqlite3_finalize(stmt);
+}
+}
+
 
 
 
 int main(){
     // Status 0 means completed 
     // 1 means not completed
+    
     
     sqlite3 *db; // Create sqlite3 object
     char* errmsg;
@@ -78,17 +116,19 @@ int main(){
         return 1;
     }
 
-    char* create_table_command = "create table if not exists todolist(Id Integer Primary Key, Task Text, Topic Text, Status Integer)";
+    char* create_table_command = "create table if not exists todolist(Id Integer Primary Key AUTOINCREMENT, Task Text, Topic Text, Status Integer)";
     rc = sqlite3_exec(db, create_table_command, 0, 0, &errmsg);
     if (rc != SQLITE_OK) {
         sqlite3_close(db);
         return 1;
     }
 
+
     char input_text[100];
 
     while(1){
-        printf(":> ");
+        //printf(":> ");
+        printf("\033[1;36m:>\033[0m ");
         fgets(input_text, sizeof(input_text), stdin);
         if (strcmp(input_text, "q\n") == 0) {
             // Ansi escpe characte to have Magenta color in terminal 
